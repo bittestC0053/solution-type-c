f3f5be15241ef0797502b3fa4e441821f018278b
BIT-USER <BIT-USER@222.106.22.54>
2019-02-28 10:52:18 +0900

    commit 2

--- src/main/java/problem04/Main.java
+++ src/main/java/problem04/Main.java
@@ -2,8 +2,12 @@ package problem04;
 
 import java.util.Scanner;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, ScriptException {
 		
 		int num1 = 0;
 		int num2 = 0;
@@ -11,11 +15,19 @@ public class Main {
 		int count =0;
 		int qucount=0;//question에 값을 넣기 위해 세는 카운트
 		
+		int user = 0;
+		
+		int[] success = new int[18];
+		int[] fail = new int[18];
+		double[] time = new double[18];
+		
+		ScriptEngineManager mgr = new ScriptEngineManager();
+		ScriptEngine engine = mgr.getEngineByName("JavaScript");
+		
 		Scanner sc = new Scanner(System.in);
 		
 		String[] question = new String[18];
 		
-		//System.out.println(question[0]=1+" * "+2);
 		
 		for(int i = 1;i<10;i++) {
 			for(int j=0;j<2;j++) {
@@ -24,7 +36,7 @@ public class Main {
 					num1=(int)(Math.random()*9)+1;
 				}else {
 					num2 = num1;
-					question[qucount] = i+" * "+num1;
+					question[qucount] = i+"*"+num1;
 					//System.out.println(question[qucount]);
 					qucount++;
 				}
@@ -37,9 +49,20 @@ public class Main {
 			long start = System.currentTimeMillis();
 			qunum=(int)(Math.random()*question.length);
 			
-			System.out.println();
+			System.out.println((count+1)+".\t"+question[qunum]+" ?");
+			
+			user = sc.nextInt();
+			
+			if(Integer.parseInt(engine.eval(question[qunum]).toString())==user) {			
+				success[count]++;
+			}else {
+				fail[count]++;
+			}
 			long end = System.currentTimeMillis();
-			System.out.println("실행시간 : "+ (end-start)/1000.0+"초");
+			time[count] = ( end - start )/1000.0 ;	
+			
+			System.out.println((count+1) +" 번째 실행시간 : "+ time[count] +"초 // 성공횟수 : "+success[count]+"실패횟수 : "+fail[count]);
+			count++;
 		}
 		
 	}
